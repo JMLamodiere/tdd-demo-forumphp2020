d71d2e4e00121164256d0a2a16634f5aba7329e7
Jean-Marie Lamodière <JMLamodiere@users.noreply.github.com>
2020-10-17 15:59:53 +0200

    move symfony controller dir and enable web profiler in dev

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
